a0f3b3d0bf36e65c4164ebc4279b8f642eb79d4f
Joel MARIN <joel.marin@neuf.fr>
2025-04-13 10:47:55 +0200

    Suppression liens vers F3FChrono

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import setuptools
-from F3FChrono.Utils import is_running_on_pi
 
 with open("README.md", "r") as fh:
 
@@ -8,9 +7,6 @@ with open("README.md", "r") as fh:
 install_requires = [
        'numpy==1.26.4', 'matplotlib', 'opencv-python==4.7.0.72', 'keyboard', 'pyserial'
     ]
-
-if not is_running_on_pi():
-    install_requires.append('fake_rpi')
 
 setuptools.setup(
     name='RobotAutonome',
