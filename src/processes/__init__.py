5225f73a01ef005e608aab5f4944e547834b8862
Timeo <fort.timeo38@gmail.com>
2026-06-28 21:43:16 +0200

    renomage du fichier de la class de base des processus en robot_process

--- src/processes/__init__.py
+++ src/processes/__init__.py
@@ -1,3 +1,3 @@
-from .base import RobotProcess, ProcessConfig, SharedResources
+from .robot_process import RobotProcess, ProcessConfig, SharedResources
 
 __all__ = ["RobotProcess", "ProcessConfig", "SharedResources"]
